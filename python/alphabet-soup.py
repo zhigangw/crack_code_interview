9615c4e537aab0e3db15da35f9e12e0a8b507d59
Zhigang Wang <zhigangw@hotmail.com>
2021-02-03 18:17:08 -0800

    alphabet-soup.py

--- python/alphabet-soup.py
+++ python/alphabet-soup.py
@@ -30,7 +30,81 @@ Solution: Topological sort of a priority graph
 
 Follow up: remove the constraint that words are all of the same length that the list may not contain any duplicate
 '''
+
 from collections import namedtuple
 from typing import Dict, List
-Graph = namedtuple()
+
+Graph = namedtuple('Graph', ['priority_graph', 'in_degree'])
+
+def init_graph(words:List[str]) -> Graph:
+    priority_graph: Dict[str, List[str]] = {}
+    in_degree: Dict[str,int] = {}
+
+    for word in words:
+        for char in word:
+            if char not in priority_graph:
+                priority_graph[char] = []
+                in_degree[char] = 0
+    return Graph(priority_graph=priority_graph, in_degree=in_degree)
+
+def fill_graph(words:List[str], graph: Graph) -> None:
+    for i in range(len(words) -1):
+        word_a = words[i]
+        word_b = words[i+1]
+
+        for j in range(len(word_a)):
+            char_a = word_a[j]
+            char_b = word_b[j]
+
+            if char_a != char_b:
+                graph.priority_graph[char_a].append(char_b)
+                graph.in_degree[char_b] += 1
+                break
+
+
+def topological_sort(graph:Graph) ->List[str]:
+    priority_graph = graph.priority_graph
+    in_degree = graph.in_degree
+    fringe:List[str] = [char for char in in_degree if in_degree[char] == 0]
+    output: List[str] = []
+
+    while fringe:
+        char = fringe.pop()
+        output.append(char)
+
+        for other_char in priority_graph.get(char, []):
+            in_degree[other_char] -= 1
+            if in_degree[other_char] == 0:
+                fringe.append(other_char)
+    return output
+
+def extract_alphabet(words:List[str]) -> List[str]:
+    graph: Graph = init_graph(words)
+    fill_graph(words, graph)
+    return topological_sort(graph)
+
+TEST_CASE = [
+    {
+        'input': ["ct", "bt", "tb"],
+        'output': ['c', 'b', 't']
+    },
+    {
+        'input': ["cc", "cb", "bb", "ac"],
+        'output': ['c', 'b', 'a']
+    },
+    {
+        'input': ["cb", "cg", "db", "dx", "dg", "bg"],
+        'output': ['c', 'd', 'b', 'x', 'g']
+    },
+    {
+        'input': ["axc", "byc", "ccy", "ccx"],
+        'output': ['y', 'x', 'a', 'b', 'c']
+    }
+]
+for test in TEST_CASE:
+    input = test['input']
+    expected = test['output']
+    real = extract_alphabet(input)
+    print('input:' ,input, "expected:", expected,  "real:", real, "SUCCEEDED" if expected == real else "FAILED")
+
 
